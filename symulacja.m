5b2ed0e387ebba82b62328596b97939102d6abea
Quantunium <zapominacz@gmail.com>
2015-04-26 15:47:17 +0200

    System uszkodzeń

--- symulacja.m
+++ symulacja.m
@@ -1,8 +1,9 @@
+%stale czasu
 minuta = 60;
 godzina = 60 * minuta;
 dzien = 24 * godzina;
 
-nieobsluzeniKlienci = 0;
+%switch czasowy dla ruchu
 rushHours = rushHours .* godzina;
 endRushHours = endRushHours .* godzina;
 emptyHours = emptyHours .* godzina;
@@ -12,19 +13,32 @@ isEmptyHours = false;
 rushHourIndex = 1;
 emptyHourIndex = 1;
 
+%klienci
+nieobsluzeniKlienci = 0;
+calkowitaLiczbaKlientow = 0;
+iloscKlientow = 0;
+oczekujacych = 0;
+
 %przygotowywanie potraw
 przygotowanychPotraw = 0;
-oczekujacych = 0;
 doUkonczeniaPotrawy = wblrnd(1.158774415699941e+02, 1.3222556979404211, 1,  kucharzy);
 
-
-iloscKlientow = 0;
-calkowitaLiczbaKlientow = 0;
+%kasy
 czasDoNastepnegoKlienta = 0;
 czasDoParagonuKas = zeros(1,iloscKas);
+
+%uszkodzenia kas
+czasDoUszkodzenia = exprnd(1/(3*godzina), 1, iloscKas);
+%naprawy
+czasDoNaprawy = zeros(1,iloscKas);
+%status 0 - dziala, 1 - nie
+statusKas = zeros(1,iloscKas);
+
+%czas
 dniSymulacji = 0;
 czasDnia = 11 * dzien;
 
+%symulacja
 while(dniSymulacji < iloscDniSymulacji)
     %koniec dnia
     if(czasDnia > 22 * godzina)
@@ -41,6 +55,10 @@ while(dniSymulacji < iloscDniSymulacji)
         isRushHours = false;
         czasDnia = 11 * godzina;
         dniSymulacji = dniSymulacji + 1;
+        %uszkodzenia kas
+        czasDoUszkodzenia = exprnd(1/(3*godzina), 1, iloscKas);
+        %naprawy
+        czasDoNaprawy = zeros(1,iloscKas);
         continue;
     end
     
@@ -74,10 +92,24 @@ while(dniSymulacji < iloscDniSymulacji)
     
     %obsluga kas
     for kasa = 1:iloscKas
-        if(czasDoParagonuKas(1, kasa) <= 0 && iloscKlientow > 0)
+        if(czasDoParagonuKas(1, kasa) <= 0 && iloscKlientow > 0 && statusKas(1, kasa) == 0)
             iloscKlientow = iloscKlientow -1;
             oczekujacych = oczekujacych + 1;
             czasDoParagonuKas(1, kasa) = (1+ (oczekujacych )/15) * lognrnd(4.186137273240221, 0.582386104269140);
+        end
+        %psucie sie kas
+        if(czasDoUszkodzenia(1, kasa) <= 0 && statusKas(1,kasa) == 0)
+            %konczy obsluge
+            %if(czasDoParagonuKas(1, kasa) > 0)
+            %    iloscKlientow = iloscKlientow -1;
+            %    oczekujacych = oczekujacych + 1;
+            %end
+            statusKas(1,kasa) = 1;
+            %tylko 1 narazie - paragon
+            czasDoNaprawy(1,kasa) = wblrnd(30, 2.1);
+        elseif(czasDoNaprawy(1, kasa) <= 0 && statusKas(1,kasa) == 1)
+            czasDoUszkodzenia(1, kasa) = exprnd(1/(3*godzina));
+            statusKas(1,kasa) = 0;
         end
     end
     
@@ -115,11 +147,13 @@ while(dniSymulacji < iloscDniSymulacji)
     end
     
     %nastepny event
-    eventTime = [czasDoNastepnegoKlienta, czasDoParagonuKas, doUkonczeniaPotrawy];
+    eventTime = [czasDoNastepnegoKlienta, czasDoParagonuKas, doUkonczeniaPotrawy, czasDoUszkodzenia, czasDoNaprawy];
     nextEvent = min(eventTime(eventTime > 0));
     czasDnia = nextEvent + czasDnia;
     %skrocenie czasow
     doUkonczeniaPotrawy = doUkonczeniaPotrawy - nextEvent;
     czasDoNastepnegoKlienta = czasDoNastepnegoKlienta - nextEvent;
     czasDoParagonuKas = czasDoParagonuKas - nextEvent;
+    czasDoNaprawy = czasDoNaprawy - nextEvent;
+    czasDoUszkodzenia = czasDoUszkodzenia - nextEvent;
 end
